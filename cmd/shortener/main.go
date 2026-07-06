ede0ff7fdc1ae5f8b7d36c736dea8a7ca1e971f1
krasov <magneticmechanicals@gmail.com>
2026-07-06 17:11:34 +0300

    init gin app

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	const Port int = 8080
+	router := gin.Default()
+
+	router.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{})
+	})
+
+	if err := router.Run(fmt.Sprintf(":%d", Port)); err != nil {
+		panic(err)
+	}
 }
