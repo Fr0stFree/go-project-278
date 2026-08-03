62a901c43c1cdd056a579e371cfb764b1872c3d3
krasov <magneticmechanicals@gmail.com>
2026-08-03 13:37:37 +0300

    feat(shortener): update LinkVisit schema

--- internal/services/shortener/types.go
+++ internal/services/shortener/types.go
@@ -13,9 +13,9 @@ type LinkVisit struct {
 	ID        int    `json:"id"`
 	LinkID    int    `json:"link_id"`
 	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	UpdatedAt string `json:"-"`
 	IP        string `json:"ip"`
 	UserAgent string `json:"user_agent"`
 	Status    int    `json:"status"`
-	Referrer  string `json:"referrer"`
+	Referrer  string `json:"-"`
 }
