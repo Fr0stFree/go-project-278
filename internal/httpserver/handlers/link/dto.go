d202354d68f6f040e637fc4da98472decebfabca
krasov <magneticmechanicals@gmail.com>
2026-09-13 17:21:51 +0300

    fix(tests): fix integration tests

--- internal/httpserver/handlers/link/dto.go
+++ internal/httpserver/handlers/link/dto.go
@@ -6,7 +6,7 @@ import (
 
 type createLinkRequestBody struct {
 	OriginalURL string `json:"original_url" binding:"required,http_url"`
-	ShortName   string `json:"short_name" binding:"min=5,max=20,alphanum,omitempty"`
+	ShortName   string `json:"short_name" binding:"omitempty,min=5,max=20,alphanum"`
 }
 
 type createLinkResponseBody shortener.Link
